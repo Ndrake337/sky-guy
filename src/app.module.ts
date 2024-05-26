ecb4bc66d33dbcbfea721a282b17a8a42935b3c6
Matheus Felipe Neves Campos Cardoso <matheus.felipe337@gmail.com>
2024-05-25 23:01:22 -0300

    chore: adjust folder structure pattern

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { AppController } from './infra/http/controllers/app.controller';
 import { AppService } from './app.service';
 
 @Module({
